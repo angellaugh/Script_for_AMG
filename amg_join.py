cf4ad91a66ddf777daadd1320d18db90e7be35ce
eric zhang <ericnomail@gmail.com>
2013-02-26 10:18:20 +0800

    create a repository for AMG related
    small tools for personal usage.

---
+++ amg_join.py
@@ -0,0 +1,75 @@
+﻿# python2.5.1
+# author : eric zhang
+# email  : ericnomail@gmail.com
+# twitter: @loveisbug
+#    http://weibo.com/loveisbug
+# date        version    PIC    comments
+# 20110720    0.0.1      eric   
+# 20110725    0.0.2      eric   can use, a bug "ContemporaryR&B;"(first style).
+# 20110726    0.1.0      eric   solved ';' issue, solved no style issue.
+# 20110804    0.1.1      eric   solved label split; '&' in label.
+# 20110828    0.1.2      eric   split '/' in label and remove space.
+# 20111227    0.1.3      eric   support multi '/' in label.
+# 20120529    0.2.0      eric   support new version of AMG.
+# 20120607    0.2.1      eric   get the 'CD' label.
+
+import urllib
+import string
+import HTMLParser
+from BeautifulSoup import BeautifulSoup
+
+def tag_from_amg(url):
+    # open the webpage and parse
+    html_src = urllib.urlopen(url).read()
+    parser = BeautifulSoup(html_src, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    h = HTMLParser.HTMLParser()
+    amg_str = ""
+    star = u"星"
+    half = u"半"
+    rate0_flag = 1
+    lst = []
+
+    # find rating,release date
+    rating_str = parser.find('span', 'hidden')
+    if rating_str != None:
+        rating = float(rating_str.text)
+        if rating * 2 % 2:
+            lst.append(str(string.atoi(rating_str.text[:1])) + star + half)
+        else:
+            lst.append(str(string.atoi(rating_str.text)) + star)
+    else:
+        lst.append("0" + star)
+    rel_date = parser.find('dd', 'release-date')
+    if rel_date != None:
+        lst.append(rel_date.text[-4:])
+
+    # find style tags
+    find_result = parser.findAll('dd', 'styles')
+    if len(find_result) > 0:
+        style = find_result[0]
+        list_len = len(style.findAll('li'))
+        i = 0
+        while i < list_len:
+            tmp_str = h.unescape(style.findNext('li').text)
+            lst.append(tmp_str.replace(" ", ""))
+            style = style.findNext('li')
+            i += 1
+
+    # find label
+    html_src = urllib.urlopen(url + '/releases').read()
+    parser = BeautifulSoup(html_src, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    find_result = parser.findAll('td', 'format')
+    for album in find_result:
+        if album.text == "CD":
+            tmp = album.findNext('td').text
+            label_ori = tmp.replace(album.findNext('strong').text, "")
+            label = label_ori.replace(" ", "")
+            lst.append(label)
+            mul_label = label_ori.find('/')
+            if mul_label != -1:
+                label_div = label.replace("/", " ")
+                lst.append(label_div)
+            lst.append(album.findNext('strong').findNext('td').text)
+    print ' '.join(lst)
+
+tag_from_amg('http://www.allmusic.com/album/rhymes-reasons-mw0000207709')
---
+++ get_tracks.py
@@ -0,0 +1,33 @@
+# python2.5.1
+# author : eric zhang
+# email  : ericnomail@gmail.com
+# twitter: @loveisbug
+#    http://weibo.com/loveisbug
+# date        version    PIC    comments
+# 20110826    0.0.1      eric   first version.
+# 20111011    0.1.0      eric   support multi discs.
+# 20130220    0.2.0      eric   not work for xiami, update for allmusic.
+
+import urllib
+import string
+from BeautifulSoup import BeautifulSoup
+
+def get_tracks(url):
+    parser = BeautifulSoup(urllib.urlopen(url).read())
+
+    i = 1
+    discs = parser.find('div', {'id' : 'tracks'})
+    disc_table = discs.findAll('div', 'table-container')
+    if len(disc_table) > 0:
+        i = 1
+        for disc in disc_table:
+            print "disc " + str(i)
+            i += 1
+            tracks = disc.findAll('a', 'primary_link')
+            j = 1
+            for track in tracks:
+                print str(j) + ". " + track.text
+                j += 1
+            print
+
+get_tracks('http://www.allmusic.com/album/rhymes-reasons-mw0000207709')
